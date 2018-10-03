d32fc044343ac6003c261cdc5893434873ca4bb4
Ahmed Khokar <amkhokar@gmail.com>
2018-10-03 12:51:40 -0700

    initialize backup files and set up structure

---
+++ FitnessTracker/backup.js
@@ -0,0 +1,72 @@
+var userInput = [""];
+var userList = [];
+var daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]; 
+
+var views = {
+    Use user input to graph results and have dummy information so that people can see different types of graphs
+
+};
+
+var controllers = {
+
+};
+var models = {
+    user = {
+        id: this.length - 1,
+        firstName: userInput,
+        lastName: userInput,
+        weight: userInput,
+        height: userInput,
+        phoneNumber: userInput,
+        email: userInput,
+        birthday: userInput,
+        gender: userInput,
+        add: function(){
+            userList.push(this);
+        },
+        getAll: () => userList,
+        deleteAll: () => userList = [],
+        delete: () => {
+            userList = userList.splice(this.id, this.id + 1); 
+        },
+        clearAll: () => {
+            userList = []; 
+        }
+    },
+    exercise = {
+        id: this.length - 1,
+        name: userInput,
+        add: function(){
+            workout.exerciseList.push(this.name);
+        },
+        getAll: function(){
+
+        },
+        delete: function() {
+
+        },
+        clearAll: function() {
+
+        }
+    },
+    workout = {
+        id: this.length - 1,
+        name: userInput,
+        exerciseList: [],
+        add: function() {
+        
+        },
+        getAll: function() {
+
+        },
+        delete: function() {
+
+        },
+        clearAll: function() {
+
+        }
+    },
+
+};
+
+
